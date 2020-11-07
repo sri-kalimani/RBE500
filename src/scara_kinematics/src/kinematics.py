16b8e62f0fb3b4c10498f6deafc0eb117e90e10f
sri-kalimani <skalimani@wpi.edu>
2020-11-07 09:42:33 -0500

    kinematics subscribed to joint_states

--- src/scara_kinematics/src/kinematics.py
+++ src/scara_kinematics/src/kinematics.py
@@ -2,22 +2,20 @@
 import rospy
 import math as m
 from geometry_msgs.msg import Vector3, Pose, Quaternion
+from sensor_msgs.msg import JointState
 import geometry_msgs
 
 def callback (msg):    
     pub = rospy.Publisher('pose', Pose, queue_size=10)
 
-    q1 = msg.x
-    q2 = msg.y
-    q3 = msg.z
+    joints = msg.position
+    q1 = joints[0]
+    q2 = joints[1]
+    q3 = joints[2]
+
     x= m.cos(q1)+m.cos(q1)*m.cos(q2)-m.sin(q1)*m.sin(q2)
     y= m.sin(q1)+m.cos(q1)*m.sin(q2)-m.sin(q1)*m.cos(q2)
     z=1-q3
-    # print("For l1=l2=d1=1")
-    # print("End Effector Position:")
-    # print("x= %f" %x)
-    # print("y= %f" %y)
-    # print("z= %f" %z)
 
     fk_pose = Pose()
     fk_pose.position.x = x
@@ -30,15 +28,13 @@ def callback (msg):
 
     pub.publish(fk_pose)
 
-    
-
 
 def listener():
     rospy.init_node('scara_kinematics')
     rate = rospy.Rate(2)
 
     while not rospy.is_shutdown():
-        rospy.Subscriber ('scara_joint', Vector3, callback)
+        rospy.Subscriber('joint_states', JointState, callback)
         rate.sleep()
  
     rospy.spin()
