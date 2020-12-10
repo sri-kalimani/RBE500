bad2327cbc238d90fc880a41ffb7947510ca78ec
sri-kalimani <skalimani@wpi.edu>
2020-12-09 21:17:30 -0500

    updated vel kinematics service client files

--- src/scara_kinematics/src/CalcJointVel_client.py
+++ src/scara_kinematics/src/CalcJointVel_client.py
@@ -3,40 +3,48 @@ import rospy
 import numpy as np
 import math as m
 from math import tan, cos, sin, atan, pi
-from scara_kinematics import TaskSpaceVelocities, TaskSpaceVelocitiesResponse
-
+from scara_kinematics.srv import JointSpaceVelocities
+from gazebo_msgs.srv import GetJointProperties, ApplyJointEffort, GetModelState
 from geometry_msgs.msg import Vector3, Pose
 from sensor_msgs.msg import JointState as JointStateMsg
 from std_msgs.msg import Float64 as FloatMsg
 
 from time import sleep as Sleep
 
-class CalcJointVel_client:
 
-    def __init__(self):
-        rospy.init_node('CalcJointVel_client')
 
-        #pose pub
-        #self.pose_pub = rospy.Publisher('forkin_pose', Pose, queue_size=10)
-
-        #Set up forward kinematics service
-        #self.forkin_service = rospy.Service('forkin', Forkin, self.forkin_service_handler)
-
-        #Set up gazebo subscriber
-        #rospy.Subscriber('/test_arm/joint_states', JointStateMsg, self.ForKinCallback, queue_size = 1)
-
-if __name__ == '__main__':
+def client():
     rospy.wait_for_service('gazebo/get_joint_properties')
-    #read from gazebo
+    #read joint positions from gazebo
     get_joint_property = rospy.ServiceProxy('gazebo/get_joint_properties', GetJointProperties)
-    req_1 = get_joint_property('Joint1')
-    
+    req_1 = get_joint_property('fixture')
+        
     get_joint_property = rospy.ServiceProxy('gazebo/get_joint_properties', GetJointProperties)
-    req_2 = get_joint_property('Joint2')
-    
+    req_2 = get_joint_property('dummy_rotation')
+        
     get_joint_property = rospy.ServiceProxy('gazebo/get_joint_properties', GetJointProperties)
-    req_3 = get_joint_property('Joint3')
+    req_3 = get_joint_property('d_3')
 
-    q1 = req_1.position[0]
-    q2 = req_2.position[0]
-    q3 = req_3.position[0]
+    #read task velocities from gazebo
+    get_model_state = rospy.ServiceProxy('gazebo/get_model_state', GetModelState)
+    req_4 = get_model_state('test_arm', 'ground_plane')
+
+    #create empty arrays to store values from gazebo
+    q_pos = [0] * 3
+    task_vel = [0] * 6
+
+    #insert the gazebo values
+    q_pos[0] = req_1.position[0]
+    q_pos[1] = req_2.position[0]
+    q_pos[2] = req_3.position[0]
+
+    task_vel[0] = req_4.twist.linear.x
+    task_vel[1] = req_4.twist.linear.y
+    task_vel[2] = req_4.twist.linear.z
+    task_vel[3] = req_4.twist.angular.x
+    task_vel[4] = req_4.twist.angular.y
+    task_vel[5] = req_4.twist.angular.z
+
+    return (q_pos, task_vel)
+if __name__ == '__main__':
+    print (client())
--- src/scara_kinematics/src/CalcTaskVel.py
+++ src/scara_kinematics/src/CalcTaskVel.py
@@ -3,7 +3,7 @@ import rospy
 import numpy as np
 import math as m
 from math import tan, cos, sin, atan, pi
-from scara_kinematics import TaskSpaceVelocities, TaskSpaceVelocitiesResponse
+from scara_kinematics.srv import *
 
 from geometry_msgs.msg import Vector3, Pose
 from sensor_msgs.msg import JointState as JointStateMsg
@@ -15,17 +15,11 @@ class CalcTaskVel:
 
     def __init__(self):
         rospy.init_node('CalcTaskVel')
+        self.control_service = rospy.Service('joint_velpos', TaskSpaceVelocities, self.CalcTaskVel)
 
-        #pose pub
-        #self.pose_pub = rospy.Publisher('forkin_pose', Pose, queue_size=10)
-
-        #Set up forward kinematics service
-        #self.forkin_service = rospy.Service('forkin', Forkin, self.forkin_service_handler)
-
-        #Set up gazebo subscriber
-        #rospy.Subscriber('/test_arm/joint_states', JointStateMsg, self.ForKinCallback, queue_size = 1)
-
-    def CalcTaskVel(self, q_pos, q_vel):
+    def CalcTaskVel(self, req):
+        q_pos = req.q_pos
+        q_vel = req.q_vel
         Lx = 1
         Lz = 2
         L2 = 1
@@ -34,8 +28,7 @@ class CalcTaskVel:
         theta2 = q_pos[1]
         d3 = q_pos[2]
 
-        jacob = 
-                [[-Lx*sin(theta1) - L2*cos(theta1)*sin(theta2) - L2*cos(theta2)*sin(theta1), - L2*cos(theta1)*sin(theta2) - L2*cos(theta2)*sin(theta1), 0],
+        jacob = [[-Lx*sin(theta1) - L2*cos(theta1)*sin(theta2) - L2*cos(theta2)*sin(theta1), - L2*cos(theta1)*sin(theta2) - L2*cos(theta2)*sin(theta1), 0],
                 [Lx*cos(theta1) + L2*cos(theta1)*cos(theta2) - L2*sin(theta1)*sin(theta2), L2*cos(theta1)*cos(theta2) - L2*sin(theta1)*sin(theta2), 0],
                 [0, 0, -1],
                 [0, 0, 0],
@@ -44,17 +37,16 @@ class CalcTaskVel:
         
         joint_vel = np.array(q_vel[0], q_vel[1],q_vel[2]).reshape(3,1)
         task_vel = np.matmul(jacob,joint_vel)
+        x = task_vel[0]
+        y = task_vel[1]
+        z = task_vel[2]
+        wx = task_vel[3]
+        wy = task_vel[4]
+        wz = task_vel[5]
 
-        return task_vel
+        return TaskSpaceVelocitiesResponse(x,y,z,wx,wy,wz)
 
 if __name__ == '__main__':
-    #Wait for serivce stuff
-    rospy.wait_for_service('joint_velpos')
-    try:
-        mvr = rospy.ServiceProxy('joint_velpos', TaskSpaceVelocities)
-        resp = mvr(ref)
-        return (resp.q_pos , resp.q_vel)
-    except rospy.ServiceException as e:
-        print("Service failed")
+    ctv = CalcTaskVel()
     rospy.spin()
 
